b4048183405f61c8d9e2514fcd6619d82e141c59
gstojsic <goran.stojsic@gmail.com>
2019-06-08 23:26:14 +0200

    cleanup and fix

--- src/main/kotlin/org/skunkworks/foundationdb/FoundationDbManager.kt
+++ src/main/kotlin/org/skunkworks/foundationdb/FoundationDbManager.kt
@@ -1,10 +1,13 @@
 package org.skunkworks.foundationdb
 
 import com.apple.foundationdb.FDB
+import com.apple.foundationdb.Transaction
 import com.apple.foundationdb.tuple.Tuple
 import kotlinx.coroutines.future.await
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.cbor.Cbor
+import java.nio.ByteBuffer
+import java.util.concurrent.CompletableFuture
 import kotlin.reflect.KClass
 import kotlin.reflect.full.companionObject
 import kotlin.reflect.full.companionObjectInstance
@@ -16,33 +19,32 @@ class FoundationDbManager {
     private val serializers = mutableMapOf<String, KSerializer<*>>()
     private val cbor = Cbor()
 
-//    suspend fun execute() {
-//        writeDbAsync("hello", "Bananas")
-//
-//        val o = Data(1, Payload("gorem ipsum dolor sit amet"))
-//        write("mellow", o)
-//
-//        val mellow: Data? = read("mellow")
-//
-//        println("Mellow $mellow")
-//
-//        val bytes = readDb("hello")
-//        if (bytes === null) {
-//            println("Key not found")
-//            return
-//        }
-//        val hello = Tuple.fromBytes(bytes).getString(0)
-//        println("Hello $hello")
-//    }
-
-//    private suspend fun readDb(key: String): ByteArray? = db.readAsync { return@readAsync it[Tuple.from(key).pack()] }.await()
+    init {
+        db.options().also {
+            it.setTransactionTimeout(2000)
+            it.setTransactionRetryLimit(5)
+        }
+    }
 
     suspend inline fun <reified T : Any> read(key: String) = read(key, T::class)
 
     suspend fun <T : Any> read(key: String, kClass: KClass<T>): T? {
-        val className = kClass.simpleName ?: throw RuntimeException("No classname")
-        val serializer = getSerializer(className, kClass)
-        return readObjectDb(key, serializer as KSerializer<T?>)
+        val bytes = readBytes(key)
+        if (bytes === null)
+            return null
+        //Tuple.fromBytes(Tuple.fromBytes(bytes).getBytes(0)).getString(0)
+        return when (kClass) {
+            String::class -> Tuple.fromBytes(bytes).getString(0) as T
+            Int::class -> decodeInt(Tuple.fromBytes(bytes).getBytes(0)) as T
+            Long::class -> Tuple.fromBytes(bytes).getLong(0) as T
+            Boolean::class -> Tuple.fromBytes(bytes).getBoolean(0) as T
+            Double::class -> Tuple.fromBytes(bytes).getDouble(0) as T
+            else -> {
+                val className = kClass.simpleName ?: throw RuntimeException("No classname")
+                val serializer = getSerializer(className, kClass)
+                deserializeObject(bytes, serializer as KSerializer<T?>)
+            }
+        }
     }
 
     private fun <T : Any> getSerializer(className: String, kClass: KClass<out T>): KSerializer<*> {
@@ -53,32 +55,125 @@ class FoundationDbManager {
         }
     }
 
-    private suspend fun <T> readObjectDb(key: String, serializer: KSerializer<T>): T? {
-        val bytes = readBytes(key)
+    private fun <T> deserializeObject(bytes: ByteArray?, serializer: KSerializer<T>): T? {
+        return if (bytes === null)
+            null
+        else cbor.load(serializer, bytes)
+    }
+
+    //    private suspend fun readBytes(key: String): ByteArray? =
+//            db.readAsync { return@readAsync it[Tuple.from(key).pack()] }.await()
+    private suspend fun readBytes(key: String): ByteArray? {
+        return readWithRetry { it[Tuple.from(key).pack()] }
+//        db.createTransaction().use { tr ->
+//            return tr[Tuple.from(key).pack()].await()
+//        }
+    }
+
+    private suspend fun readWithRetry(block: (Transaction) -> CompletableFuture<ByteArray?>): ByteArray? {
+        db.createTransaction().use { tr ->
+            var t = tr
+            var res: ByteArray?
+            while (true) {
+                try {
+                    res = block(t).await()
+                } catch (e: RuntimeException) {
+                    t = t.onError(e).await()
+                    continue
+                }
+                return res
+            }
+        }
+        return null
+    }
+
+    private suspend fun writeBytesDbAsync(key: String, value: ByteArray) {
+        updateWithRetry { it[Tuple.from(key).pack()] = value }
+    }
+
+    suspend fun <T : Any> write(key: String, o: T?) {
+        if (o === null) {
+            writeBytesDbAsync(key, byteArrayOf())
+            return
+        }
+
+        val bytes = when (val kClass = o::class) {
+            String::class, Long::class, Boolean::class, Double::class -> Tuple.from(o).pack()
+            Int::class -> encodeInt(o as Int)
+            else -> {
+                val className = kClass.simpleName ?: throw RuntimeException("No classname")
+                val serializer = getSerializer(className, kClass)
+                cbor.dump(serializer as KSerializer<T>, o)
+            }
+        }
+        writeBytesDbAsync(key, bytes)
+    }
+
+    suspend fun clear(key: String) {
+        updateWithRetry { it.clear(Tuple.from(key).pack()) }
+    }
+
+    private suspend fun updateWithRetry(block: (Transaction) -> Unit) {
+        db.createTransaction().use { tr ->
+            var t = tr
+            while (true) {
+                try {
+                    //t.clear(Tuple.from(key).pack())
+                    block(t)
+                    t.commit().await()
+                    break
+                } catch (e: RuntimeException) {
+                    t = t.onError(e).await()
+                }
+            }
+        }
+    }
+
+    suspend fun clear(startKey: String, endKey: String) {
+        updateWithRetry { it.clear(Tuple.from(startKey).pack(), Tuple.from(endKey).pack()) }
+    }
+
+    inline fun <reified T : Any> readBlocking(key: String) = readBlocking(key, T::class)
+
+    fun <T : Any> readBlocking(key: String, kClass: KClass<T>): T? {
+        val className = kClass.simpleName ?: throw RuntimeException("No classname")
+        val serializer = getSerializer(className, kClass)
+        return readObjectBlocking(key, serializer as KSerializer<T?>)
+    }
+
+    private fun <T> readObjectBlocking(key: String, serializer: KSerializer<T>): T? {
+        val bytes = readBytesBlocking(key)
         return if (bytes === null)
             null
         else cbor.load(serializer, Tuple.fromBytes(bytes).getBytes(0))
     }
 
-    private suspend inline fun readBytes(key: String): ByteArray? =
-            db.readAsync { return@readAsync it[Tuple.from(key).pack()] }.await()
+    private fun readBytesBlocking(key: String): ByteArray? =
+            db.read { return@read it[Tuple.from(key).pack()] }.join()
 
-    //    private suspend fun writeDbAsync(key: String, value: String) =
-//            db.runAsync {
-//                it[Tuple.from(key).pack()] = Tuple.from(value).pack()
-//                return@runAsync CompletableFuture.completedFuture(null)
-//            }.await()
-//
-    private suspend fun writeBytesDbAsync(key: String, value: ByteArray): Void? {
-        val tr = db.createTransaction()
-        tr[Tuple.from(key).pack()] = Tuple.from(value).pack()
-        return tr.commit().whenComplete { _, _ -> tr.close() }.await()
-    }
-
-    suspend fun <T : Any> write(key: String, o: T): Void? {
+    fun <T : Any> writeBlocking(key: String, o: T) {
         val kClass = o::class
         val className = kClass.simpleName ?: throw RuntimeException("No classname")
         val serializer = getSerializer(className, kClass)
-        return writeBytesDbAsync(key, cbor.dump(serializer as KSerializer<T>, o))
+        writeBytesBlock(key, cbor.dump(serializer as KSerializer<T>, o))
+    }
+
+    private fun writeBytesBlock(key: String, value: ByteArray) {
+        db.createTransaction().use { tr ->
+            tr[Tuple.from(key).pack()] = Tuple.from(value).pack()
+            tr.commit().join()
+        }
+    }
+
+    private fun encodeInt(value: Int): ByteArray {
+        val output = ByteArray(4)
+        ByteBuffer.wrap(output).putInt(value)
+        return output
+    }
+
+    private fun decodeInt(value: ByteArray): Int {
+        if (value.size != 4)
+            throw IllegalArgumentException("Array must be of size 4")
+        return ByteBuffer.wrap(value).int
     }
 }
--- src/main/kotlin/org/skunkworks/foundationdb/main.kt
+++ src/main/kotlin/org/skunkworks/foundationdb/main.kt
@@ -1,18 +1,78 @@
 package org.skunkworks.foundationdb
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.runBlocking
 import org.skunkworks.foundationdb.serialization.Data
 import org.skunkworks.foundationdb.serialization.Payload
+import kotlin.system.measureTimeMillis
+
+const val key = "mellow"
+const val limit = 100000
+//const val limit = 10
+val manager = FoundationDbManager()
 
 fun main() {
-    val manager = FoundationDbManager()
+//    var data : List<Data?> = emptyList()
+//    val time = measureTimeMillis {
+//        data = (1..limit).map { i ->
+//            val k = "$key-$i"
+//            val mellowOriginal = Data(i, Payload("gorem ipsum dolor sit amet $i"))
+//            manager.writeBlocking(k, mellowOriginal)
+//
+//            manager.readBlocking(k) as Data?
+//        }
+//    }
+//    println("Data: $data")
+//    println("Time: $time")
 
     runBlocking {
-        val mellowOriginal = Data(1, Payload("gorem ipsum dolor sit amet"))
-        manager.write("mellow", mellowOriginal)
+        //manager.clear("$key-1", "$key-$limit")
 
-        val mellow: Data? = manager.read("mellow")
+        val timeErase = measureTimeMillis {
+            (1..limit).map { i ->
+                val k = "$key-$i"
+                async { manager.clear(k) }
+            }.joinAll()
+        }
+        println("Time erase: $timeErase")
 
-        println("Mellow $mellow")
+//        var data: List<Data?> = readData(manager)
+//        println("Count after erase: ${data.count()}")
+
+        val timeWrite = measureTimeMillis {
+            (1..limit).map { i ->
+                val k = "$key-$i"
+                val mellowOriginal = Data(i, Payload("gorem ipsum dolor sit amet $i"))
+//                val mellowOriginal = "gorem ipsum dolor sit amet $i"
+                async { manager.write(k, mellowOriginal) }
+            }.joinAll()
+        }
+        println("Time write: $timeWrite")
+        println("Data write/sec: ${limit.toDouble() * 1000 / timeWrite.toDouble()}")
+
+        var data: List<Data?> = emptyList()
+//        var data: List<String?> = emptyList()
+        val timeRead = measureTimeMillis {
+            data = readData(manager)
+        }
+
+        println("Data: $data")
+        println("Data size: ${data.count()}")
+        println("Time read: $timeRead")
+        println("Data read/sec: ${data.count().toDouble() * 1000 / timeRead.toDouble()}")
+        println("Total time: ${timeRead + timeWrite}")
     }
+}
+
+private suspend fun readData(manager: FoundationDbManager): List<Data?> {
+//private suspend fun readData(manager: FoundationDbManager): List<String?> {
+    val jobs = (1..limit).map { i ->
+        val k = "$key-$i"
+        coroutineScope { async { manager.read(k) as Data? } }
+//        coroutineScope { async { manager.read(k) as String? } }
+    }
+    jobs.joinAll()
+    return jobs.map { it.await() }.filter { it !== null }
 }
