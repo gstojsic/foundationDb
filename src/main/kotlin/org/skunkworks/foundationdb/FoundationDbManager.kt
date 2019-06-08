f70438b7c16f53f8baa5e25070799a34f93ecc2e
gstojsic <goran.stojsic@gmail.com>
2019-06-08 10:40:23 +0200

    serialization

--- src/main/kotlin/org/skunkworks/foundationdb/FoundationDbManager.kt
+++ src/main/kotlin/org/skunkworks/foundationdb/FoundationDbManager.kt
@@ -1,8 +1,16 @@
 package org.skunkworks.foundationdb
 
+import kotlinx.serialization.Serializable
+
 import com.apple.foundationdb.FDB
 import com.apple.foundationdb.tuple.Tuple
 import kotlinx.coroutines.future.await
+import kotlinx.serialization.ContextualSerialization
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.modules.serializersModule
+import java.util.*
 import java.util.concurrent.CompletableFuture
 
 
@@ -11,7 +19,7 @@ class FoundationDbManager {
     private val db = fdb.open()
 
     suspend fun execute() {
-        writeDbAsync("hello", "Neeeee")
+        writeDbAsync("hello", "Daaaaa")
 
         val bytes = readDb("hello")
         if (bytes === null) {
@@ -21,14 +29,20 @@ class FoundationDbManager {
 
         val hello = Tuple.fromBytes(bytes).getString(0)
         println("Hello $hello")
+
+        val o = Data(1, Payload("lorem ipsum dolor sit amet"))
+        val jsonConfiguration = JsonConfiguration(
+                prettyPrint = true
+        )
+        val json = Json(configuration = jsonConfiguration)
+        println(json.stringify(Data.serializer(), o))
     }
 
     private suspend fun readDb(key: String): ByteArray? = db.readAsync { return@readAsync it[Tuple.from(key).pack()] }.await()
     private suspend fun writeDbAsync(key: String, value: String) =
             db.runAsync {
-                return@runAsync CompletableFuture.supplyAsync {
-                    it[Tuple.from(key).pack()] = Tuple.from(value).pack()
-                }
+                it[Tuple.from(key).pack()] = Tuple.from(value).pack()
+                return@runAsync CompletableFuture.completedFuture(null)
             }.await()
 
     private fun writeDb(key: String, value: String) =
@@ -36,12 +50,15 @@ class FoundationDbManager {
                 it[Tuple.from(key).pack()] = Tuple.from(value).pack()
             }
 
-//    @Serializable
-//    data class Data(
-//            val id: Int,
-//            @Serializable val payload: Payload
-//    )
-//
-//    data class Payload(val content: String)
+    @Serializable
+    data class Data(
+            val id: Int,
+            @Serializable(with = IX::class)val payload: Payload
+//            @ContextualSerialization val payload: Payload
+    )
 
+    data class Payload(val content: String)
+
+    @Serializer(forClass = Payload::class)
+    object IX
 }
