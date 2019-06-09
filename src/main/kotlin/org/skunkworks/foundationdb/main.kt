7f5678007f1351cb2d8051b5b0b187220b74ff9b
gstojsic <goran.stojsic@gmail.com>
2019-06-09 23:19:21 +0200

    variations

--- src/main/kotlin/org/skunkworks/foundationdb/main.kt
+++ src/main/kotlin/org/skunkworks/foundationdb/main.kt
@@ -8,16 +8,17 @@ import org.skunkworks.foundationdb.serialization.Data
 import org.skunkworks.foundationdb.serialization.Payload
 import kotlin.system.measureTimeMillis
 
-const val key = "mellow"
+const val keyPrefix = "mellow"
 const val limit = 100000
 //const val limit = 10
+val message = "a".repeat(100)
 val manager = FoundationDbManager()
 
 fun main() {
 //    var data : List<Data?> = emptyList()
 //    val time = measureTimeMillis {
 //        data = (1..limit).map { i ->
-//            val k = "$key-$i"
+//            val k = "$keyPrefix-$i"
 //            val mellowOriginal = Data(i, Payload("gorem ipsum dolor sit amet $i"))
 //            manager.writeBlocking(k, mellowOriginal)
 //
@@ -28,13 +29,13 @@ fun main() {
 //    println("Time: $time")
 
     runBlocking {
-        //manager.clear("$key-1", "$key-$limit")
 
         val timeErase = measureTimeMillis {
-            (1..limit).map { i ->
-                val k = "$key-$i"
-                async { manager.clear(k) }
-            }.joinAll()
+            manager.clearRange(keyPrefix)
+//            (1..limit).map { i ->
+//                val k = "$keyPrefix-$i"
+//                async { manager.clear(k) }
+//            }.joinAll()
         }
         println("Time erase: $timeErase")
 
@@ -43,9 +44,10 @@ fun main() {
 
         val timeWrite = measureTimeMillis {
             (1..limit).map { i ->
-                val k = "$key-$i"
-                val mellowOriginal = Data(i, Payload("gorem ipsum dolor sit amet $i"))
-//                val mellowOriginal = "gorem ipsum dolor sit amet $i"
+                val k = "$keyPrefix-$i"
+                val mellowOriginal = Data(i, Payload(message))
+                //val mellowOriginal = Data(i, Payload("gorem ipsum dolor sit amet $i"))
+                //val mellowOriginal = "gorem ipsum dolor sit amet $i"
                 async { manager.write(k, mellowOriginal) }
             }.joinAll()
         }
@@ -58,7 +60,7 @@ fun main() {
             data = readData(manager)
         }
 
-        println("Data: $data")
+        //println("Data: $data")
         println("Data size: ${data.count()}")
         println("Time read: $timeRead")
         println("Data read/sec: ${data.count().toDouble() * 1000 / timeRead.toDouble()}")
@@ -69,7 +71,7 @@ fun main() {
 private suspend fun readData(manager: FoundationDbManager): List<Data?> {
 //private suspend fun readData(manager: FoundationDbManager): List<String?> {
     val jobs = (1..limit).map { i ->
-        val k = "$key-$i"
+        val k = "$keyPrefix-$i"
         coroutineScope { async { manager.read(k) as Data? } }
 //        coroutineScope { async { manager.read(k) as String? } }
     }
